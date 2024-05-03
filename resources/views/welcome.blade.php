7b07da241775c2e0366ca8d3fc3887864d492034
kilian frederix <kilianfrederix@gmail.com>
2024-05-03 16:54:41 +0200

    nog 1 test gedaan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,19 @@
+{{-- agenda plugin --}}
+
 <link href="https://www.huurkalender.nl/online/embed/huurkalender.css" rel="stylesheet" />
 <div class="huurkalender-embed huurkalender-embed-container_calendar">
     <iframe id="iframe_huurkalender_17249"
         src="https://www.huurkalender.nl/vacancy/boeking-knipkracht-17249.html?type=iframe&lang=nl" frameborder="0"
         width="100%" allowfullscreen></iframe>
+    <script type="text/javascript" src="https://www.huurkalender.nl/online/embed/huurkalender.js"></script>
+</div>
+
+
+{{-- boeken plugin --}}
+<link href="https://www.huurkalender.nl/online/embed/huurkalender.css" rel="stylesheet" />
+<div class="huurkalender-embed huurkalender-embed-container_booking">
+    <iframe id="iframe_huurkalender_booking3_17249"
+        src="https://www.huurkalender.nl/vacancy/booking/boeking-knipkracht-17249.html?type=iframe&lang=nl"
+        frameborder="0" width="100%" allowfullscreen></iframe>
     <script type="text/javascript" src="https://www.huurkalender.nl/online/embed/huurkalender.js"></script>
 </div>
