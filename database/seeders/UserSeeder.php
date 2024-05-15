50af10d6dabf34b1350a980bdbdc356fbc712600
kilian frederix <kilianfrederix@gmail.com>
2024-05-15 15:19:00 +0200

    a

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,28 +18,28 @@ class UserSeeder extends Seeder
             [
                 'name' => 'Susie',
                 'email' => 'SusieKnipKracht@gmail.com',
-                'password' => 'test',
+                'password' => '$2y$12$whFenjIEJbXyWlMQNZwFtOeOnCbfLQMnaS1arMaQKgRfG8vE88xwu',
                 'username' => 'Susie',
                 'is_employee' => 1,
             ],
             [
                 'name' => 'Michelle',
                 'email' => 'MichelleKnipKracht@gmail.com',
-                'password' => 'test',
+                'password' => '$2y$12$BS6v4WoeU3lsBp/9ssta6.LR/n6JEFBFUPY88mP23jZFA/./sKyLe',
                 'username' => 'Michelle',
                 'is_employee' => 1,
             ],
             [
                 'name' => 'Vicky',
                 'email' => 'VickyKnipKracht@gmail.com',
-                'password' => 'test',
+                'password' => '$2y$12$.1fS0A.Gg4s8DxEb31fr9uGLa7YtzQFjymzsk1JnfOHm3Q5mh5y7C',
                 'username' => 'Vicky',
                 'is_employee' => 1,
             ],
             [
                 'name' => 'Ocean',
                 'email' => 'OceanKnipKracht@gmail.com',
-                'password' => 'test',
+                'password' => '$2y$12$DRU2Aj9RljsQZb..9ZCXV.6SmqENHwZ0ZZQfcX9kXagzcoz8l/OJa',
                 'username' => 'Ocean',
                 'is_employee' => 1,
             ]
